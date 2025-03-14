eac75282a0e90671056d6e88a075b982c961c141
Ismail Taha <eng.isma.j.taha@gmail.com>
2025-03-14 22:23:49 +0200

    Resolved merge conflict in app.component.ts and kept only new changes

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { HeaderComponent } from './Components/header/header.component';
-=======
->>>>>>> 05c252d67367c7e9626d7a87bac2f0c0122f73b3
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet,HeaderComponent],
-=======
-  imports: [RouterOutlet],
->>>>>>> 05c252d67367c7e9626d7a87bac2f0c0122f73b3
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-flask';
